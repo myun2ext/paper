94b7f0a2b3732f9dd3bffc3747f42adfcca090b4
myun2 <myun2@nwhite.info>
2015-06-19 20:28:10 +0900

    jump!!

--- paperengine.cpp
+++ paperengine.cpp
@@ -8,30 +8,39 @@ public:
 	d3ddev &d;
 	kb_input &kb;
 	sprite img1;
-	inertia<-3, 3> ix, iy;
-	float x, y;
+	inertia<-3, 3> ix;
+	inertia<0, 14>  iy;
+	float x;
+	limited<float, 0, 440> y;
 
 	renderer(d3ddev &d_in, kb_input &kb_in)
 		: d(d_in), kb(kb_in),
-		  img1(d, "point.bmp") { x = y = 0; }
+		  img1(d, "point.bmp")
+	{
+		x = y = 0;
+	}
 
 	void render()
 	{
-		ix.attenuate(0.05f); iy.attenuate(0.05f);
-
+		ix.attenuate(0.05f);
 		if ( kb.test(DIK_LEFT) )
 			ix.increase(-0.1f);
 		if ( kb.test(DIK_RIGHT) )
 			ix.increase(0.1f);
 		x += ix;
 
-		if ( kb.test(DIK_UP) )
-			y -= 1;
-		if ( kb.test(DIK_DOWN) )
-			y += 1;
+		iy.attenuate(0.1f);
+		if ( kb.test(DIK_UP) ) {
+			if ( iy < 8 )
+				iy = 8;
+			iy.increase(0.3f);
+		}
+		y -= (8 - iy);
+		if ( y == 440 )
+			iy = 8;
 
 		d.clear(0xff000033);
-		img1.render(x, y);
+		img1.render(x, 440 - y);
 	}
 };
 
